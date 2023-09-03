1a6cc0b4d967cf5589dd087268077bdd0f380419
Marcos Theofilo <matheusferraresi@live.com>
2023-09-03 13:50:43 +0100

    feat: adding parallax effect on canopy

---
+++ Assets/Scripts/Misc/Parallax.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float parallaxOffset = -0.15f;
+    
+    private Camera _camera;
+    private Vector2 _startPosition;
+    
+    private Vector2 Travel => (Vector2)_camera.transform.position - _startPosition;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = _startPosition + Travel * parallaxOffset;
+    }
+}
